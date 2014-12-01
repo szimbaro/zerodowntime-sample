dd6849badbab0a115296419346e0a4e40d5a1762
Saulo Zimbaro <saulozimbaro@gmail.com>
2014-12-01 09:01:04 -0200

    first commit

---
+++ net/start.sh
@@ -0,0 +1,13 @@
+cd /tmp
+
+rm -rf zerodowntime-sample; true
+
+git clone https://github.com/szimbaro/zerodowntime-sample.git
+
+cd zerodowntime-sample
+
+python -m SimpleHTTPServer 8080
+
+#npm install
+
+#nodejs app.js  
